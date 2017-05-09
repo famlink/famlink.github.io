0682a232baaa397e9b9a4753df7acf36419cde0a
mabuyo <mabuyo@ualberta.ca>
2017-05-09 16:06:00 -0600

    Added Mailchimp integration.

--- assets/js/main.js
+++ assets/js/main.js
@@ -139,35 +139,35 @@
 
 			// Events.
 			// Note: If you're *not* using AJAX, get rid of this event listener.
-				$form.addEventListener('submit', function(event) {
-
-					event.stopPropagation();
-					event.preventDefault();
-
-					// Hide message.
-						$message._hide();
-
-					// Disable submit.
-						$submit.disabled = true;
-
-					// Process form.
-					// Note: Doesn't actually do anything yet (other than report back with a "thank you"),
-					// but there's enough here to piece together a working AJAX submission call that does.
-						window.setTimeout(function() {
-
-							// Reset form.
-								$form.reset();
-
-							// Enable submit.
-								$submit.disabled = false;
-
-							// Show message.
-								$message._show('success', 'Thank you!');
-								//$message._show('failure', 'Something went wrong. Please try again.');
-
-						}, 750);
-
-				});
+				// $form.addEventListener('submit', function(event) {
+				//
+				// 	// event.stopPropagation();
+				// 	// event.preventDefault();
+				//
+				// 	// Hide message.
+				// 		$message._hide();
+				//
+				// 	// Disable submit.
+				// 		$submit.disabled = true;
+				//
+				// 	// Process form.
+				// 	// Note: Doesn't actually do anything yet (other than report back with a "thank you"),
+				// 	// but there's enough here to piece together a working AJAX submission call that does.
+				// 		window.setTimeout(function() {
+				//
+				// 			// Reset form.
+				// 				$form.reset();
+				//
+				// 			// Enable submit.
+				// 				$submit.disabled = false;
+				//
+				// 			// Show message.
+				// 				$message._show('success', 'Thank you!');
+				// 				//$message._show('failure', 'Something went wrong. Please try again.');
+				//
+				// 		}, 750);
+				//
+				// });
 
 		})();
 
